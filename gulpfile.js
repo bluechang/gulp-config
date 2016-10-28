5ba4da59fc0bd753ec976719cd164de87b3a11f1
bluechang <1193055507@qq.com>
2016-10-28 14:55:04 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
 
 
-const gulp 			= require('gulp');
-const del        	= require('del');
+const gulp 			= require('gulp');		
+const clean        	= require('gulp-clean');
 const less          = require('gulp-less');
 const autoprefixer	= require('gulp-autoprefixer');
 const uglify        = require('gulp-uglify');
-const rename        = require('gulp-rename');
 const imagemin      = require('gulp-imagemin');
+const rename        = require('gulp-rename');
 const gulpSequence  = require('gulp-sequence');
 const browserSync   = require('browser-sync').create();
 
@@ -42,13 +42,19 @@ const paths = {
 };
 
 
+
+gulp.task('clean', ()=>{
+	 return gulp.src(paths.base.dest, {read: false})
+	 			.pipe(clean());
+});
+
 gulp.task('html', ()=>{
-	return gulp.src(paths.html.src)
+	 return gulp.src(paths.html.src)
 				.pipe(gulp.dest(paths.html.dest));
 });
 
 gulp.task('less', ()=>{
-	return gulp.src(paths.less.main)
+	 return gulp.src(paths.less.main)
 				.pipe(less())
 				.pipe(autoprefixer({
 					browsers: ['Chrome > 0', 'ff > 0', 'ie > 0', 'Opera > 0', 'iOS > 0', 'Android > 0']
@@ -57,7 +63,7 @@ gulp.task('less', ()=>{
 });
 
 gulp.task('js', ()=>{
-	return gulp.src(paths.js.src)
+	 return gulp.src(paths.js.src)
 				.pipe(gulp.dest(paths.js.dest))
 				.pipe(uglify())
 				.pipe(rename({suffix: '.min'}))
@@ -65,18 +71,14 @@ gulp.task('js', ()=>{
 });
 
 gulp.task('lib', ()=>{
-	return gulp.src(paths.lib.src)
+	 return gulp.src(paths.lib.src)
 				.pipe(gulp.dest(paths.lib.dest));
 });
 
 gulp.task('image', ()=>{
-	return gulp.src(paths.image.src)
+	 return gulp.src(paths.image.src)
 				.pipe(imagemin())
 				.pipe(gulp.dest(paths.image.dest));
-});
-
-gulp.task('clean', ()=>{
-	return del([paths.base.dest]);
 });
 
 gulp.task('watch', ()=>{
@@ -84,7 +86,7 @@ gulp.task('watch', ()=>{
 	gulp.watch(paths.less.src, ['less']);
 	gulp.watch(paths.js.src, ['js']);
 	gulp.watch(paths.image.src, ['image']);
-	gulp.watch(paths.base.destAll, browserSync.reload);
+	gulp.watch(paths.base.srcAll, browserSync.reload);
 });
 
 gulp.task('server', ()=>{
@@ -102,14 +104,5 @@ gulp.task('build', (cb)=>{
 });
 
 gulp.task('default', ['build']);
-
-
-
-
-
-
-
-
-
 
 

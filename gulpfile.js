fd7f9230638f0eb1348c554a03209064731f4ae1
bluechang <1193055507@qq.com>
2016-10-28 14:58:02 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 
 const gulp 			= require('gulp');		
-const clean        	= require('gulp-clean');
-const less          = require('gulp-less');
-const autoprefixer	= require('gulp-autoprefixer');
+const clean 		= require('gulp-clean');
+const less 			= require('gulp-less');
+const autoprefixer 	= require('gulp-autoprefixer');
 const uglify        = require('gulp-uglify');
 const imagemin      = require('gulp-imagemin');
 const rename        = require('gulp-rename');
